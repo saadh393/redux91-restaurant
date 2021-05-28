ae6f9d1f48e31fe3fe5116afeaddab6c3a57ca5c
Saad Hasan <saadh393@gmail.com>
2021-05-28 15:36:12 +0600

    Dirctory Structure is Ready now

---
+++ src/pages/Home.js
@@ -0,0 +1,20 @@
+/*  
+💥 Title : Home
+📃 Description : 
+✍ Author : Saad Hasan
+⌚ Date : 28/ May/ 2021 
+*/
+
+
+/*  🔥 React Dependencies 🔥 */
+
+
+const Home = () => {
+    return (
+        <>
+            Hello
+        </>
+    );
+};
+
+export default Home
